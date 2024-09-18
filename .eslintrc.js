136bd84ab75961ca2246d318ec92bb1ebdd5ac61
Drac Zihper <draczihper@gmail.com>
2024-09-18 17:18:04 +0300

    Add eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base",
+        "prettier",
+    ],
+    "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+    "rules": {
+        // override default options
+        
+    }
+}
